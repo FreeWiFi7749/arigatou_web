80175c4308c910440dde5dee5738d99d208af715
FreeWiFi <131158009+FreeWiFi7749@users.noreply.github.com>
2024-02-25 14:03:00 -0600

    fix copy url

--- share.js
+++ share.js
@@ -8,7 +8,12 @@ document.getElementById('lineShare').addEventListener('click', () => {
     window.open(shareUrl, '_blank');
   });
 
-document.getElementById('lineShare').addEventListener('click', () => {
-    const shareUrl = `https://line.me/R/msg/text/?${encodeURIComponent(document.location.href)}`;
-    window.open(shareUrl, '_blank');
+  document.getElementById('copyUrl').addEventListener('click', () => {
+    navigator.clipboard.writeText(document.location.href).then(() => {
+      console.log('URLがクリップボードにコピーされました。');
+      alert('URLがクリップボードにコピーされました。');
+    }).catch(err => {
+      console.error('URLをクリップボードにコピーできませんでした。', err);
+      alert('エラー')
+    });
   });
