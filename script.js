c8f0d262c0a15d68c474d5b2e4034b41e4858d20
FreeWiFi <131158009+FreeWiFi7749@users.noreply.github.com>
2024-02-25 12:49:28 -0600

    add script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+<script>
+document.addEventListener('DOMContentLoaded', () => {
+    const thankYou = document.querySelector('.thank-you');
+    const animations = [
+        'spin 1s linear infinite',
+        'bounce 2s infinite',
+        'pulse 3s infinite ease-in-out',
+        'sway 5s infinite ease-in-out'
+    ];
+
+    const randomAnimation = animations[Math.floor(Math.random() * animations.length)];
+    thankYou.style.animation = randomAnimation;
+});
+</script>
