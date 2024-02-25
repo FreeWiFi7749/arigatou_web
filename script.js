3ecc6465ee6aed495b350f57fe24d1cea623868b
FreeWiFi <131158009+FreeWiFi7749@users.noreply.github.com>
2024-02-25 13:52:27 -0600

    add share

--- script.js
+++ script.js
@@ -21,17 +21,3 @@ document.addEventListener('DOMContentLoaded', () => {
     thankYou.style.animation = randomAnimation;
 });
 
-document.getElementById('twitterShare').addEventListener('click', () => {
-    const shareUrl = `https://twitter.com/intent/tweet?url=${encodeURIComponent(document.location.href)}&text=${encodeURIComponent('ページのタイトルや共有したいテキスト')}`;
-    window.open(shareUrl, '_blank');
-  });
-
-document.getElementById('lineShare').addEventListener('click', () => {
-    const shareUrl = `https://line.me/R/msg/text/?${encodeURIComponent(document.location.href)}`;
-    window.open(shareUrl, '_blank');
-  });
-
-document.getElementById('lineShare').addEventListener('click', () => {
-    const shareUrl = `https://line.me/R/msg/text/?${encodeURIComponent(document.location.href)}`;
-    window.open(shareUrl, '_blank');
-  });
